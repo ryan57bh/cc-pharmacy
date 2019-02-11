823848e6ac76ab9e1bfcb8460688478dafc77d7a
ryan57hb <ryan57@uic.edu>
2019-02-10 19:12:09 -0600

    fix: move trailing linebreaker

--- run_unittest.sh
+++ run_unittest.sh
@@ -3,4 +3,4 @@
 #this file will run the test for the relevant functions in pharm_counting.py
 #use run.sh to run the source file pharmacy_unittest.py first
 
-python ./src/pharm_unittest.py ./insight_testsuite/tests/unittest/input/itcont.txt ./insight_testsuite/tests/unittest/output/top_cost_drug.txt
+python ./src/pharm_unittest.py ./insight_testsuite/tests/unittest/input/test_input.txt ./insight_testsuite/tests/unittest/output/test_output.txt
